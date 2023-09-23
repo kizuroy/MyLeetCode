22177544a0f2c788b4a7e0c0d39009751b4df093
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2023-09-23 19:44:53 +0200

    Update 392. Is Subsequence.py

--- Python/392. Is Subsequence.py
+++ Python/392. Is Subsequence.py
@@ -1,12 +1,12 @@
 def isSubsequence(s: str, t: str) -> bool:
-        i = j = 0  # Initialize pointers for s and t
+        i = j = 0
 
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1  # Move the pointer in s if a match is found
             j += 1  # Always move the pointer in t
 
-        return i == len(s)  # If i reached the end of s, it is a subsequence
+        return i == len(s)  # If i reached the end is subsequence
 
 
 aa = "abc"
