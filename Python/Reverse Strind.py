5036170dbbb04a90222855fd0b2f9bf978eba7ac
Alex Veljak <32106420+kizuroy@users.noreply.github.com>
2023-11-07 23:26:44 +0100

    2 leetcode probles back at it

--- Python/Reverse Strind.py
+++ Python/Reverse Strind.py
@@ -1,3 +1,8 @@
-s = ["h","e","l","l","o"]
-print(s[::-1])
+def reverseString(self, s: list[str]) -> None:
+    Len = len(s)
+    for i in range(Len//2):
+        s[i], s[Len-1-i] = s[Len-1-i], s[i]
+    #we can use also
+    # return s[::-1]
+    
 
